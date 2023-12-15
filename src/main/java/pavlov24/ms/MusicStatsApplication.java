a70c24dcc5cbf6fc5e58f18acc064f074e3366d0
pavlov24 <pavlovmark24@gmail.com>
2023-12-15 21:24:17 +0500

    Initial commit

---
+++ src/main/java/pavlov24/ms/MusicStatsApplication.java
@@ -0,0 +1,13 @@
+package pavlov24.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pavlov24/ms/MusicStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package pavlov24.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
