ca084db85ab86b8b16c356e70b7cabf493a1eddc
pavlov24 <pavlovmark24@gmail.com>
2023-12-20 20:53:32 +0500

    Улучшил навигацию

--- src/main/java/pavlov24/ms/controller/IndexController.java
+++ src/main/java/pavlov24/ms/controller/IndexController.java
@@ -1,13 +1,21 @@
 package pavlov24.ms.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
 
 @Controller
 public class IndexController {
 
     @GetMapping()
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("links", Map.of(
+                "Исполнители", "artists/",
+                "Альбомы", "albums/",
+                "Жанры", "genres/"
+                ));
         return "index";
     }
 
